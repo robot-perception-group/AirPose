afd33f9ed1621c61c92b6c386e52b26283405718
Nitin Saini <nitin.saini@tuebingen.mpg.de>
2021-12-16 14:28:53 +0100

    typo in data preprocessing corrected

--- copenet/src/copenet/scripts/prepare_aerialpeople_dataset.py
+++ copenet/src/copenet/scripts/prepare_aerialpeople_dataset.py
@@ -11,6 +11,6 @@ train_ds = [os.path.join(data_root,*x.split("/")[-4:]) for x in train_ds]
 test_ds = [os.path.join(data_root,*x.split("/")[-4:]) for x in test_ds]
 
 pkl.dump(train_ds,open(os.path.join(data_root,"dataset","train_pkls.pkl"),"wb"))
-pkl.dump(train_ds,open(os.path.join(data_root,"dataset","test_pkls.pkl"),"wb"))
+pkl.dump(test_ds,open(os.path.join(data_root,"dataset","test_pkls.pkl"),"wb"))
 
 print("done!!!")
